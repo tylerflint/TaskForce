179e3c5ce47cd9a7b95ba39ad5b0d9a0b5efbade
Tyler Flint <tylerflint@Delorum10.local>
2010-10-10 21:01:42 -0600

    creating initial rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskforce::Application.config.secret_token = 'c07106dcb68882560f24178d81a54c88205552434091ee211774d924b87a26ec5326f10bdee69651cc3403bfb185d7c270eb0e314ef1364e06a6130c5b229d08'
